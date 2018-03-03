bfb6b6a3726a9e5e2e3b71ae4ae717e91dca28c7
Josh <jhint2424@gmail.com>
2018-03-03 15:16:11 -0700

    3 3 commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+//create variables
+var currentQuestion = 0;
+var thatsCorrect = 0;
+var thatsIncorrect = 0;
+var notAnswered = 0;
+
+var questions = //create list of questions
+[{
+
+    question: "What is Michael Scott's middle name?",
+    choices: ["Allen", "Gary", "Orville", "Mykonos"],
+    correctAnswer: 1,
+    image: ("") 
+},
+{
+    question: "What is the title of the movie in which Michael Scott wrote, directed and starred?",
+    choices: ["The Flenderson Files", "Fool me Once", "I Declare Bankruptcy", "Threat Level Midnight"],
+    correctAnswer: 3,
+    image: ("")
+}];
+$(document).ready(function()
+{   
+//clear start button when clicked
+$(".start-game-button").click(function()
+{
+    $(this).hide();
+    $(".trivia-question").text(this.questions.question);
+    $(".trivia-answers").text("answers");
+    
+});
+//call the first question and choices
+//start a timer for question
+//once user clicks an answer or timer runs out, show answer
+//call next question with no user inpout
+//add point for correct, incorrect, unanswered question
+//display the first question
+
+
+
+})
